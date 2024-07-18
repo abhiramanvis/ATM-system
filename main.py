c437c4aed6ca353546d2f2ff0b09288a44608117
abhiramanvis <av@abhiraman.com>
2024-07-18 16:04:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,3 +24,28 @@ def login_screen():
   window.close()
 
 login_screen()
+# method to the main screen
+def main_screen(acc_no,pin):
+  sg.theme('DarkTeal12')   # add a touch of color
+  # all the stuff inside your window.
+  layout = [  [sg.Text('ATM system',size=(20,2),font=('Arial', 25),justification='center')],
+              [sg.Button('Check Balance',size=(20,2)), sg.Button('Change PIN',size=(20,2)) ],
+              [sg.Button('Withdraw Amount',size=(20,2)), sg.Button('Transfer Funds', size=(20,2))],
+              [sg.Button('Deposit Amount',size=(20,2)), sg.Button('Transaction History', size=(20,2))],
+              [sg.Text('',size=(15,2)), sg.Button('Logout', size=(10,2)) ]]
+
+  # create the Window
+  window = sg.Window('Main Screen', layout)
+  # event Loop to process "events" and get the "values" of the inputs
+  while True:
+    event, values = window.read()
+    balance = 0
+    if event == sg.WIN_CLOSED: # if the user closes the window or clicks cancel
+      break
+    # check for logout
+    if event == "Logout":
+      # close the active window
+      window.close()
+      login_screen()  
+# close the active window
+  window.close()
